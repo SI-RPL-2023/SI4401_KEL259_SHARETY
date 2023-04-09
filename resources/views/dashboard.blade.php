bf1e65645fb58ab88e399696b16489d2bb0d766d
RAMADANI29 <ramdonut@student.telkomuniversity.ac.id>
2023-04-09 23:54:27 +0700

    Progress tentang kami

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -137,12 +137,14 @@
     </div>
     <!-- end galang dana -->
     <div class="wrapper">
-  <h1>-- About Section --</h1>
-  <div class="about-section">
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel nam harum fuga adipisci facilis pariatur reiciendis dicta distinctio, beatae laudantium nemo dolor? Quia, quibusdam ex doloribus illum ipsa earum odio repudiandae eligendi dicta commodi dolores.
-      <br />
+  <div class="about-section container mt-5 shadow-sm bg-body-tertiary rounded" style="margin-bottom: 50px;">
+  <center>
+  <!-- <h1>-- About Section --</h1> -->
+  <h3 class="mb-4 pt-4 px-3">Tentang Kami</h3>
+    <p style="padding-bottom:20px">
+    Sharety merupakan sebuah website donasi yang dibuat untuk memudahkan para donatur di Indonesia. Sharety dapat diakses via website sehingga memudahkan para donatur karena tidak perlu datang langsung untuk melakukan kebaikan. terdiri dari berbagai fitur yang tentunya dibuat sebaik mungkin agar tidak membingungkan pengguna website.
     </p>
+</center>
   </div>
 </div>
 
