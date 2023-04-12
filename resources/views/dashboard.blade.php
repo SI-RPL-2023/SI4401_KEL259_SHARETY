679489bceabb417435008b5600c3db02b298aa87
RAMADANI29 <ramdonut@student.telkomuniversity.ac.id>
2023-04-12 15:02:07 +0700

    no message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,7 @@
                         <a class="nav-link" href="#">Berita</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Tentang Kami</a>
+                        <a class="nav-link" href="TentangKami.blade.php">Tentang Kami</a>
                     </li>
                 </ul>
                 <span class="navbar-text">
